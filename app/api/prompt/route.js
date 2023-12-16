ecc9ae0f4be65b23702c4e74784dcaabc03e93e1
harizspreco <hariz.spreco@gmail.com>
2023-12-16 20:08:36 +0100

    cache problem fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,20 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async (request) => {
-    try {
-        await connectToDB();
-        const prompts = await Prompt.find({}).populate('creator');
+export const GET = async (req, res) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find().populate("creator");
 
-        await revalidatePath(request.url);
-        return new Response(JSON.stringify(prompts), {status: 200});
-    } catch (error) {
-        return new Response('Failed to fetch all prompts', {status: 500});
-    }
-}
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+        "cache-control":
+          "private, no-cache, no-store, max-age=0, must-revalidate",
+      },
+    });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
